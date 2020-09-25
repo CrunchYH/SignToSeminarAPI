5979cbc800898f83dcc8ae3d2a6b0a172ffe785a
Maria Nordin <miamarianordin@gmail.com>
2020-09-25 15:26:31 +0200

    Added Seminars- and users controllers

---
+++ SignToSeminarAPI/Controllers/SeminarsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignToSeminarAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeminarsController : ControllerBase
+    {
+        // GET: api/Seminars
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Seminars/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Seminars
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Seminars/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
