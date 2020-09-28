2081d5fe0f9a0835936b45303f6796654a5b8ce2
Maria Nordin <miamarianordin@gmail.com>
2020-09-28 14:02:30 +0200

    Changed get definition

--- SignToSeminarAPI/Controllers/UsersController.cs
+++ SignToSeminarAPI/Controllers/UsersController.cs
@@ -19,7 +19,7 @@ namespace SignToSeminarAPI.Controllers
         }
 
         // GET: api/Users/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
